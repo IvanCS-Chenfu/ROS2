db01e54d8dc9a93f585595c21ef5055bb56f6b06
IvanCS-Chenfu <152430268+IvanCS-Chenfu@users.noreply.github.com>
2025-10-23 15:42:09 +0200

    Add files via upload

---
+++ paquete_python/paquete_python/publisher.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class Clase_Publisher(Node):
+    def __init__(self):
+        super().__init__("nodo_publisher")
+        
+        # Creamos el objeto del publicador añadiendo el tipo de mensaje, el tópico en el que se publicará y el tamaño del buffer.
+        self.objeto_publisher = self.create_publisher(Twist,"/nombre_topico/publisher",10) 
+
+        msg = Twist()   # Creamos el mensaje tipo Twist.
+        
+        # Damos valores
+        msg.linear.x = 1.0
+        msg.linear.y = 2.0
+        msg.linear.z = 3.0
+        msg.angular.x = 4.0
+        msg.angular.y = 5.0
+        msg.angular.z = 6.0
+        
+        self.objeto_publisher.publish(msg)  # Publicamos el mensaje tipo Twist en el tópico dicho anteriormente.
+    
+def main(args=None):
+    rclpy.init(args=args)
+    
+    objeto_nodo = Clase_Publisher()
+    rclpy.spin(objeto_nodo) 
+    
+    rclpy.shutdown() 
