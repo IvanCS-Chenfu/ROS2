2b8b4da386e090d615f6983bcc1f9bd0a31c8e9b
IvanCS-Chenfu <152430268+IvanCS-Chenfu@users.noreply.github.com>
2025-10-23 15:41:51 +0200

    Change log message in nodo.py
    Updated log message from 'Baboleiro' to 'Bamboleiro'.

--- paquete_python/paquete_python/nodo.py
+++ paquete_python/paquete_python/nodo.py
@@ -7,12 +7,13 @@ class Clase_Nodo(Node):             # Clase que creamos heredando una clase de r
     def __init__(self):
         super().__init__("nombre_nodo")     # Aquí insertamos el nombre del nodo
         
-        self.get_logger().info("Baboleiro")
+        self.get_logger().info("Bamboleiro")
 
 def main(args=None):
     rclpy.init(args=args)       # Inicia ROS2
     
     objeto_nodo = Clase_Nodo()  # Creamos el objeto del Nodo
+    rclpy.spin(objeto_nodo)     # Hace  que al llamar al nodo en la terminal, este no se cierre a menos que se pulte ctr+c
     
     rclpy.shutdown()            # Cierra ROS2
 
